74843ad2cf6cd727ea2213d86c850493d5828c56
Geoffrey Sneddon <me@gsnedders.com>
2015-11-25 16:28:29 +0000

    The big 1.0 rewrite and drop of the 0.x formats.

--- pytest_expect/expect.py
+++ pytest_expect/expect.py
@@ -10,13 +10,9 @@ the normal Python format, as Python 2 only allows ASCII characters in
 identifiers.
 """
 
-import copy
-import pickle
-import pickletools
-
 import pytest
 import umsgpack
-from six import PY2, PY3, PY34, text_type, binary_type
+from six import PY2, PY3, text_type, binary_type
 
 
 def pytest_addoption(parser):
@@ -43,7 +39,8 @@ def pytest_addoption(parser):
 def pytest_configure(config):
     exp = ExpectationPlugin(config)
     config.pluginmanager.register(exp)
-    exp.load_expectations()
+    if not exp.update_xfail:
+        exp.load_expectations()
 
 
 class ExpectationPlugin(object):
@@ -53,45 +50,56 @@ class ExpectationPlugin(object):
         self.update_xfail = config.option.update_xfail
         self.warn_on_python_xfail = config.option.warn_on_python_xfail
         self.fails = set()
+        self.expect_xfail = set()
 
         if self.xfail_file is None:
             self.xfail_file = config.rootdir.join(".pytest.expect").strpath
 
     def load_expectations(self):
-        if self.update_xfail:
-            self.fails = set()
+        with open(self.xfail_file, "rb") as fp:
+            state = umsgpack.unpack(fp)
+
+        if PY3 and b'py_version' in state:
+            for key in list(state.keys()):
+                state[key.decode("ASCII")] = state[key]
+                del state[key]
+
+        version = state["version"]
+        py_version = state["py_version"]
+
+        if version >= 0x0200:
+            self.expect_xfail = set()
+            self.config.warn("W1", "test expectation file in unsupported version")
+        elif version >= 0x0100:
+            xfail = state["expect_xfail"]
+            self.expect_xfail = set()
+            for s in xfail:
+                self.expect_xfail.add(s)
+                if PY3 and py_version == 2 and isinstance(s, binary_type):
+                    self.expect_xfail.add(s.decode('latin1'))
+                elif PY2 and py_version == 3 and isinstance(s, text_type):
+                    try:
+                        self.expect_xfail.add(s.encode("latin1"))
+                    except UnicodeEncodeError:
+                        pass
         else:
-            try:
-                with open(self.xfail_file, "rb") as fp:
-                    if PY3:
-                        try:
-                            expectations = pickle.load(fp, encoding="bytes")
-                        except LookupError:
-                            expectations = pickle.load(fp, encoding="latin1")
-                    else:
-                        expectations = pickle.load(fp)
-            except IOError:
-                self.to_mark = set()
-            except _VersionError:
-                self.config.warn("W1", "test expectation file in unsupported version")
-                self.to_mark = set()
-            else:
-                self.to_mark = expectations.expect_xfail
+            self.expect_xfail = set()
+            self.config.warn("W1", "test expectation file in unsupported version")
 
     def pytest_collectreport(self, report):
         passed = report.outcome in ('passed', 'skipped')
         if passed and not self.update_xfail:
-            if report.nodeid in self.to_mark:
-                self.to_mark.remove(report.nodeid)
+            if report.nodeid in self.expect_xfail:
+                self.expect_xfail.remove(report.nodeid)
                 for item in report.result:
-                    self.to_mark.add(item.nodeid)
+                    self.expect_xfail.add(item.nodeid)
         elif not passed and self.update_xfail:
             self.fails.add(report.nodeid)
 
     def pytest_collection_modifyitems(self, session, config, items):
         if not self.update_xfail:
             for item in items:
-                if item.nodeid in self.to_mark:
+                if item.nodeid in self.expect_xfail:
                     item.add_marker(pytest.mark.xfail)
 
     def pytest_runtest_logreport(self, report):
@@ -100,83 +108,9 @@ class ExpectationPlugin(object):
 
     def pytest_sessionfinish(self, session):
         if self.update_xfail and not hasattr(session.config, 'slaveinput'):
-            expectation = _Expectations(self.fails)
-            pickled = pickle.dumps(expectation, protocol=2)
+            state = {}
+            state["py_version"] = 2 if PY2 else 3
+            state["version"] = 0x0100
+            state["expect_xfail"] = list(self.fails)
             with open(self.xfail_file, "wb") as fp:
-                fp.write(pickletools.optimize(pickled))
-
-
-class _Expectations(object):
-    def __init__(self, expect_xfail):
-        assert isinstance(expect_xfail, set)
-
-        self.py_version = 2 if PY2 else 3
-        self.expect_xfail = expect_xfail
-
-    def __getstate__(self):
-        d = copy.copy(self.__dict__)
-        d["version"] = 0x0300
-        d["expect_xfail"] = list(d["expect_xfail"])
-        return umsgpack.packb(d)
-
-    def __setstate__(self, state):
-        if isinstance(state, binary_type):
-            state = umsgpack.unpackb(state)
-        elif isinstance(state, text_type):
-            state = umsgpack.unpackb(state.decode('latin1'))
-
-        if PY3 and b'py_version' in state:
-            for key in list(state.keys()):
-                state[key.decode("ASCII")] = state[key]
-                del state[key]
-
-        version = state["version"]
-        del state["version"]
-
-        self.__dict__ = state
-
-        if version >= 0x0400:
-            raise _VersionError
-        elif version >= 0x0300:
-            xfail = self.expect_xfail
-            self.expect_xfail = set()
-            for s in xfail:
-                self.expect_xfail.add(s)
-                if PY3 and self.py_version == 2 and isinstance(s, binary_type):
-                    self.expect_xfail.add(s.decode('latin1'))
-                elif PY2 and self.py_version == 3 and isinstance(s, text_type):
-                    try:
-                        self.expect_xfail.add(s.encode("latin1"))
-                    except UnicodeEncodeError:
-                        pass
-        elif version >= 0x0200:
-            xfail = self.expect_xfail
-            self.expect_xfail = set()
-            for t, s in xfail:
-                self.expect_xfail.add(s)
-                if PY3 and self.py_version == 2 and t == b'b':
-                    if PY34:
-                        self.expect_xfail.add(s.decode('latin1'))
-                    else:
-                        try:
-                            self.expect_xfail.add(s.encode("latin1"))
-                        except UnicodeEncodeError:
-                            pass
-                elif PY2 and self.py_version == 3 and t == b'u':
-                    try:
-                        self.expect_xfail.add(s.encode("latin1"))
-                    except UnicodeEncodeError:
-                        pass
-        elif version >= 0x0100:
-            if ((PY3 and not PY34 and self.py_version == 2) or
-                    (PY2 and self.py_version == 3)):
-                for x in copy.copy(self.expect_xfail):
-                    if isinstance(x, text_type):
-                        try:
-                            self.expect_xfail.add(x.encode("latin1"))
-                        except UnicodeEncodeError:
-                            pass
-
-
-class _VersionError(Exception):
-    pass
+                umsgpack.pack(state, fp)
